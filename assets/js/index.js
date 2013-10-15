bad9cbc3065d4b5e6b48317d43eff04b65cae7e2
zutrinken <xoxo@peteramende.de>
2013-10-16 01:55:18 +0200

    made theme ajax ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,116 @@
-(function ($) {
-    "use strict";
+jQuery(function($) {
 
-    $(document).ready(function(){
-    
-    	$('.post-content').fitVids();
+	/* ==========================================================================
+	   Fitvids by Chris Coyier
+	   ========================================================================== */
 
+	$('#wrapper').fitVids();
+
+	/* ==========================================================================
+	   Ajax Loading by Rory Gibson
+	   ========================================================================== */
+
+    var History = window.History;
+    var loading = false;
+    var showIndex = false;
+    var $ajaxContainer = $('#ajax-container');
+    var $latestPost = $('#latest-post');
+    var $postIndex = $('#post-index');
+
+    // Initially hide the index and show the latest post
+    $latestPost.show();
+    $postIndex.hide();
+
+    // Show the index if the url has "page" in it (a simple
+    // way of checking if we're on a paginated page.)
+    if (window.location.pathname.indexOf('page') === 1) {
+        $latestPost.hide();
+        $postIndex.show();
+    }
+
+    // Check if history is enabled for the browser
+    if ( ! History.enabled) {
+        return false;
+    }
+
+    History.Adapter.bind(window, 'statechange', function() {
+        var State = History.getState();
+
+        // Get the requested url and replace the current content
+        // with the loaded content
+        $.get(State.url, function(result) {
+            var $html = $(result);
+            var $newContent = $('#ajax-container', $html).contents();
+
+            $('html, body').animate({'scrollTop': 0});
+
+            $ajaxContainer.fadeOut(500, function() {
+                $latestPost = $newContent.filter('#latest-post');
+                $postIndex = $newContent.filter('#post-index');
+
+                if (showIndex === true) {
+                    $latestPost.hide();
+                } else {
+                    $latestPost.show();
+                    $postIndex.hide();
+                }
+
+                $ajaxContainer.html($newContent);
+                $ajaxContainer.fadeIn(500);
+
+                NProgress.done();
+
+                loading = false;
+                showIndex = false;
+            });
+        });
     });
 
-}(jQuery));
+    $('body').on('click', '.js-ajax-link, .pagination a', function(e) {
+        e.preventDefault();
+
+        if (loading === false) {
+            var currentState = History.getState();
+            var url = $(this).attr('href');
+            var title = $(this).attr('title') || null;
+
+            // If the requested url is not the current states url push
+            // the new state and make the ajax call.
+            if (url !== currentState.url.replace(/\/$/, "")) {
+                loading = true;
+
+                // Check if we need to show the post index after we've
+                // loaded the new content
+                if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
+                    showIndex = true;
+                }
+
+                NProgress.start();
+
+                History.pushState({}, title, url);
+            } else {
+                // Swap in the latest post or post index as needed
+                if ($(this).hasClass('js-show-index')) {
+                    $('html, body').animate({'scrollTop': 0});
+
+                    NProgress.start();
+
+                    $latestPost.fadeOut(300, function() {
+                        $postIndex.fadeIn(300);
+                        NProgress.done();
+                    });
+                } else {
+                    $('html, body').animate({'scrollTop': 0});
+
+                    NProgress.start();
+
+                    $postIndex.fadeOut(300, function() {
+                        $latestPost.fadeIn(300);
+                        NProgress.done();
+                    });
+                }
+            }
+        }
+    });
+
+});
